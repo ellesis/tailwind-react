56ece36151f8f71d4d8acb758f2f443ecbdbe919
Lisa Han <ellesis@gmail.com>
2022-02-15 18:53:44 -0800

    add Hamburger menu sample

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PageFood from './pages/page-food'
 import PageComponents from './pages/page-components'
 import PageEgg from './pages/page-egg'
 import PageLayout from './pages/page-layout'
+import PageMain from './pages/page-main'
 
 function App() {
   return (
@@ -15,8 +16,10 @@ function App() {
       <Route path='/components' element={<PageComponents />} />
       <Route path='/egg' element={<PageEgg />} />
       <Route path='/contact' element={<PageFood />} />
-
       <Route path='/food' element={<PageFood />} />
+
+      <Route path='/main' element={<PageMain />} />
+
       <Route path='/' element={<PageFront />} />
     </Routes>
   )
---
+++ src/pages/page-main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import NavMenuBlue from '../components/navbars/NavMenuBlue'
+
+const PageMain = () => {
+  return (
+    <main>
+      <div className='bg-indigo-800 shadow-xl m-6 rounded'>
+        <NavMenuBlue />
+      </div>
+
+      <div className='text-center'>
+        <p className='pt-5'>
+          Show me some love on{' '}
+          <a className='text-indigo-800 font-bold' href='https://twitter.com/fabio_runci'>
+            Twitter
+          </a>{' '}
+          if this has helped you
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default PageMain
