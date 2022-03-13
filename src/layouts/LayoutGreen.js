5a7abb6e58a1d30e5b06844185822d4c02e7bf31
Lisa Han <ellesis@gmail.com>
2022-03-13 08:17:00 -0700

    Add green UI

---
+++ src/layouts/LayoutGreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import NavbarGreen from '../components/navbars/NavbarGreen'
+import SideMenuNinja from '../components/sidebars/SideMenuNinja'
+
+export default function LayoutGreen({children}) {
+  const layoutTitle = 'Tailwind React'
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavbarGreen />
+      <div className="flex flex-col md:flex-row flex-1">
+        <aside className="w-full md:w-60">
+          {/* Title */}
+          <h3 className="text-right font-bold uppercase p-4 border-b border-gray-100">
+            <a href="/" className="hover:text-gray-700">
+              {layoutTitle}
+            </a>
+          </h3>
+          {/* Left Side Menu */}
+          <SideMenuNinja />
+        </aside>
+        <main className="flex-1 px-16 py-6 bg-gray-100">{children}</main>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/green.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LayoutGreen from '../layouts/LayoutGreen'
+
+export default function Green() {
+  return (
+    <LayoutGreen>
+      <div className="title">Page-Green</div>
+    </LayoutGreen>
+  )
+}
