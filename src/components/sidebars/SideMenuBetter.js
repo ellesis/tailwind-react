953ddbd367d93a53b33df069499e5a199aec1533
Lisa Han <ellesis@gmail.com>
2022-03-17 03:03:23 -0700

    [Better UI] control response left sidebar on small window

--- src/components/sidebars/SideMenuBetter.js
+++ src/components/sidebars/SideMenuBetter.js
@@ -6,10 +6,10 @@ import {menuItems} from '../../data/menu'
 export default function SideMenuBetter() {
   return (
     <nav>
-      <ul>
+      <ul className="w-full h-full">
         {menuItems.map(({href, title}) => (
-          <NavLink to={href} className={({isActive}) => (isActive ? 'flex bg-blue-700' : 'flex')}>
-            <li key={title} className="block w-full py-2.5 px-4 font-extrabold hover:bg-blue-600">
+          <NavLink key={title} to={href} className={({isActive}) => (isActive ? 'flex bg-blue-700' : 'flex')}>
+            <li className="block w-full py-2.5 px-4 font-extrabold hover:bg-blue-600">
               <span>{title}</span>
             </li>
           </NavLink>
--- src/layouts/LayoutBetter.js
+++ src/layouts/LayoutBetter.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SideMenuBetter from '../components/sidebars/SideMenuBetter'
 
 export default function LayoutBetter({children}) {
+  const [isMobile, setMobile] = useState(false)
+
+  const handleMenuBtnClick = () => {
+    setMobile(!isMobile)
+  }
+
   return (
     <div className="relative min-h-screen md:flex">
       {/* mobile menu bar */}
-      <div class="bg-gray-800 text-gray-100 flex justify-between md:hidden">
+      <div className="bg-gray-800 text-gray-100 flex justify-between md:hidden">
         {/* logo */}
-        <a href="#" class="block p-4 text-white font-bold">
+        <a href="/" className="block text-xl p-4 text-white font-bold">
           Better Dev
         </a>
         {/* mobile menu button */}
-        <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700">
-          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        <button onClick={handleMenuBtnClick} className="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
       </div>
 
       {/* side Menu */}
-      <div className="sidebar bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-ou">
+      <div
+        className={`${
+          isMobile ? 'top-0 right-0 translate-x-0' : 'md:relative md:translate-x-0'
+        } w-64 bg-blue-800 text-blue-100 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full  transition duration-200 ease-in-ou`}
+      >
         {/* logo */}
         <a href="#" className="text-white flex items-center space-x-2 px-4">
           <svg
@@ -40,20 +57,8 @@ export default function LayoutBetter({children}) {
         </a>
 
         {/* nav */}
-        <SideMenuBetter />
-        {/*
-        <nav>
-          <a href="#" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">
-            Home
-          </a>
-          <a href="" className="block py-2.5 px-4">
-            About
-          </a>
-          <a href="" className="block py-2.5 px-4"></a>
-          <a href="" className="block py-2.5 px-4"></a>
-        </nav> */}
+        <SideMenuBetter show={isMobile} />
       </div>
-
       {/* content */}
       <div className="flex-1 p-10 text-2xl">{children}</div>
     </div>
