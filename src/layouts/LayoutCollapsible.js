68ab5c8c5b2f1d46d660fb7864cb1c178f52db3b
Lisa Han <ellesis@gmail.com>
2022-03-22 21:55:39 -0700

    Layout Collapsible and CollapsibleSidebar

---
+++ src/layouts/LayoutCollapsible.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function LayoutCollapsible() {
+  return (
+    <>
+      <div class="flex">
+        <aside class="sm:w-1/5 bg-blue-500 min-h-screen"></aside>
+        <div>
+          <div>LayoutCollapsible</div>
+        </div>
+      </div>
+    </>
+  )
+}
