7473ad7dc7a659685c15e09a8bcde9ce776db938
Lisa Han <ellesis@gmail.com>
2022-03-27 05:58:29 -0700

    Add SideMenuBetter component to SideSlideMenu

--- src/components/sidebars/SideSlideMenu.js
+++ src/components/sidebars/SideSlideMenu.js
@@ -1,13 +1,19 @@
 import React, {useState} from 'react'
 
+import SideMenuBetter from './SideMenuBetter'
+
+/**
+ * slide right sidebar
+ */
 export default function SideSlideMenu() {
   const [showSidebar, setShowSidebar] = useState(false)
 
   return (
     <>
+      {/* Hamburger button or close button */}
       {showSidebar ? (
         <button
-          className="flex text-4xl text-white items-center cursor-pointer fixed right-10 top-6 z-50"
+          className="flex text-4xl text-white items-center cursor-pointer fixed right-8 top-4 z-50"
           onClick={() => setShowSidebar(!showSidebar)}
         >
           x
@@ -15,11 +21,11 @@ export default function SideSlideMenu() {
       ) : (
         <svg
           onClick={() => setShowSidebar(!showSidebar)}
-          className="fixed  z-30 flex items-center cursor-pointer right-10 top-6"
+          className="fixed z-30 flex items-center cursor-pointer right-8 top-4"
           fill="#2563EB"
           viewBox="0 0 100 80"
-          width="40"
-          height="40"
+          width="26"
+          height="26"
         >
           <rect width="100" height="10"></rect>
           <rect y="30" width="100" height="10"></rect>
@@ -27,12 +33,15 @@ export default function SideSlideMenu() {
         </svg>
       )}
 
+      {/* side menu */}
       <div
-        className={`top-0 right-0 w-[35vw] bg-blue-600  p-10 pl-20 text-white fixed h-full z-40  ease-in-out duration-300 ${
+        className={`fixed top-0 right-0 w-72 h-full p-4 bg-blue-600 text-white z-40 ease-in-out duration-300 ${
           showSidebar ? 'translate-x-0 ' : 'translate-x-full'
         }`}
       >
-        <h3 className="mt-20 text-4xl font-semibold text-white">I am a sidebar</h3>
+        <div className="mt-12">
+          <SideMenuBetter />
+        </div>
       </div>
     </>
   )
