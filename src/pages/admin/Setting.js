172f39b11565edebb58dbc4dc70d4c95ddf53554
Lisa Han <ellesis@gmail.com>
2022-04-17 06:35:33 -0800

    Make Setting page in the AdminLayout

--- src/pages/admin/Setting.js
+++ src/pages/admin/Setting.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import UserInfo from '../../components/forms/UserInfo'
+import UserProfileCard from '../../components/cards/UserProfileCard'
 
 export default function Setting() {
-  return <div className="z-100 h-40 bg-green-300">Setting</div>
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-8/12 px-4">
+          <UserInfo />
+        </div>
+        <div className="w-full lg:w-4/12 px-4">
+          <UserProfileCard />
+        </div>
+      </div>
+    </>
+  )
 }
