c783596a3e754137bae1c1b120701a48e9158c75
Lisa Han <ellesis@gmail.com>
2022-04-22 10:42:04 -0700

    Change front page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
-import Food from './pages/food'
-import Components from './pages/components'
+// single page
+import Contact from './pages/contact'
+import Front from './pages/front'
+import About from './pages/about'
+import PageChart from './pages/page_chart'
 import PageEgg from './pages/page-egg'
 import PageLayout from './pages/page-layout'
 import PageMain from './pages/page-main'
 
-import Yokose from './pages/yokose'
-import Green from './pages/green'
-
-import Contact from './pages/contact'
-import Front from './pages/front'
-import About from './pages/about'
+// Layout
 import RightSlide from './pages/right-slide'
 import Better from './pages/better'
+import Food from './pages/food'
+import Components from './pages/components'
+import Yokose from './pages/yokose'
+import Green from './pages/green'
 
 // Layout
 import AdminLayout from './layouts/AdminLayout'
@@ -22,25 +24,23 @@ import AdminLayout from './layouts/AdminLayout'
 function App() {
   return (
     <Routes>
-      <Route path="/layout" element={<PageLayout />} />
-      <Route path="/admin/*" element={<AdminLayout />} />
-
+      <Route path="/chart" element={<PageChart />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="/components" element={<Components />} />
       <Route path="/egg" element={<PageEgg />} />
       <Route path="/food" element={<Food />} />
-
       <Route path="/main" element={<PageMain />} />
 
       <Route path="/yokose" element={<Yokose />} />
       <Route path="/green" element={<Green />} />
       <Route path="/right-slide" element={<RightSlide />} />
       <Route path="/better" element={<Better />} />
-
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route path="/layout" element={<PageLayout />} />
+      <Route path="/admin/*" element={<AdminLayout />} />
       <Route path="/front" element={<Front />} />
 
-      <Route path="/" element={<AdminLayout />} />
+      <Route path="/" element={<Front />} />
     </Routes>
   )
 }
