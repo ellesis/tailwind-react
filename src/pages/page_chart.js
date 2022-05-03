0e8532537186d95273ea206006f67dd90b60f21a
Lisa Han <ellesis@gmail.com>
2022-05-03 10:18:04 -0700

    add chart samples

--- src/pages/page_chart.js
+++ src/pages/page_chart.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 import LayoutOrange from '../layouts/LayoutOrange'
 
+import LineChart from '../components/charts/LineChart'
+import BarChart from '../components/charts/BarChart'
+
 /**
  * echart components sample
  */
 export default function PageChart() {
   return (
     <LayoutOrange>
-      <div>PageChart</div>
+      <h2 className="mb-4">eChart Samples</h2>
+      <LineChart />
+      <BarChart />
     </LayoutOrange>
   )
 }
