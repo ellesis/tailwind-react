13bc18f7b9fb5399f6ca810880fd1f634485659d
Lisa Han <ellesis@gmail.com>
2022-05-29 05:43:55 -0800

    Fix avatar image url

--- src/components/cards/AvatarCard.js
+++ src/components/cards/AvatarCard.js
@@ -3,7 +3,11 @@ import React from 'react'
 const AvatarCard = () => {
   return (
     <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-      <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="img/avatar.jpg" alt="Avatar Image" />
+      <img
+        className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={require('../../assets/img/avatar/girl.png')}
+        alt="Avatar Image"
+      />
       <div className="text-center space-y-2 sm:text-left">
         <div className="space-y-0.5">
           <p className="text-lg text-black font-semibold">Erin Lind</p>
